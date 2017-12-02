49abd778823726195f0f265313b1ea56c4dfa56e
Remco de Vos <vos.remcode@gmail.com>
2017-12-02 08:47:14 +0100

    Slight change to base class and added final documentation

--- src/visualisations/basevisualisation.js
+++ src/visualisations/basevisualisation.js
@@ -14,17 +14,21 @@ class BaseVisualisation {
     }
 
     /**
-     * 
+     * Base update function, called when the visualisation needs to be updated as the data 
+     * has changed or the filtered data has changed.
      * @param {Object[]} data - The collection of data
      * @param {Object[]} filtered_data - The collection of filtered data
+     * @param {boolean} data_has_changed - Boolean indicating if the data has been changed from last time
      */
-    update(data, filtered_data) {
-        this.data = data;
+    update(data, filtered_data, data_has_changed = false) {
+        if (data_has_changed) {
+            this.data = data;
+        }
         this.filtered_data = filtered_data;
     }
 
     /**
-     * 
+     * Filter function which filters out the data which is excluded based on the filters selection.
      * @param {Object[]} data - The data to filter
      * @returns {Object[]} - The filtered data from the visualisation
      */
