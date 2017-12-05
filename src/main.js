8e93d71c3225af40ed57d8ef5d3c6f8ae5ee89e7
Remco de Vos <vos.remcode@gmail.com>
2017-12-05 16:48:30 +0100

    Integers are no longer strings but parsed to integers

--- src/main.js
+++ src/main.js
@@ -6,9 +6,17 @@ import data_url from '../data/data.csv';
 var data = [];
 var visualisations = [];
 
+var numeric_value_properties = ["Count", "Score", "ViewCount", "AnswerCount", "CommentCount", "FavoriteCount"];
+
 d3.csv(data_url, (d) => {
     data = d;
+    data.forEach(data_element => {
+        numeric_value_properties.forEach(id => {
+            data_element[id] = parseInt(data_element[id]);
+        });
+    });
     var graph = new TagGraph(d3.select("#taggraph"), data, {});
     visualisations.push(graph);
+    console.log(data);
 });
 
