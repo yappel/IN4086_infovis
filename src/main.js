733cc0359a38c0a2120cf9d4bbc4c23e36909781
Remco de Vos <vos.remcode@gmail.com>
2017-12-05 14:35:54 +0100

    Webpack loads d3 correctly and puts the bundle in the index.html

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import * as d3 from "d3";
+import BaseVisualisation from "./visualisations/basevisualisation.js";
+
+var root = {};
+var bv = new BaseVisualisation(root, []);
+
+d3.csv("../data/data.csv", function(error, data) {
+    
+    data.forEach(function(d) {
+        d.date = d3.timeParse("%Y-%m").parse;
+        d.value = +d.value;
+    });
+
+    console.log("Data processed");
+
+}); 
