82aec2cc4e84a4264fb9500cdfc0aadab74913fc
Remco de Vos <vos.remcode@gmail.com>
2017-12-05 14:08:15 +0100

    Added html loading to webpack and dist creation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -19,5 +20,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
 };
