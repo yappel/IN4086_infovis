92c0069c46316405858cddfadb19f4d256f80b1b
Yoeri Appel <y.a.appel@studen.tudelf.nl>
2017-12-05 17:00:54 +0100

    Fixed csv file name

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 import BaseVisualisation from "./visualisations/basevisualisation.js";
 import TagGraph from './visualisations/taggraph.js';
-import data_url from '../data/data.csv';
+import data_url from '../data/QueryResults.csv';
 
 var data = [];
 var visualisations = [];
