027272a5a7dcbac1069c967f5e640e83aad79327
Yoeri Appel <y.a.appel@studen.tudelf.nl>
2017-12-05 16:49:38 +0100

    Made barchart work with our datasource

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-console.log(2);
 var margin = {top: 20, right: 20, bottom: 70, left: 40},
 width = 600 - margin.left - margin.right,
-height = 300 - margin.top - margin.bottom;
-
-// Parse the date / time
-var	parseDate = d3.time.format("%Y-%m").parse;
+height = 800 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
 
@@ -12,8 +8,7 @@ var y = d3.scale.linear().range([height, 0]);
 
 var xAxis = d3.svg.axis()
 .scale(x)
-.orient("bottom")
-.tickFormat(d3.time.format("%Y-%m"));
+.orient("bottom");
 
 var yAxis = d3.svg.axis()
 .scale(y)
@@ -27,15 +22,62 @@ var svg = d3.select("body").append("svg")
 .attr("transform", 
       "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("../data/bar-data.csv", function(error, data) {
+var tag = function(dataEntry) {
+  var self = {};
+  var userIds = {}
+
+  self.TagName = dataEntry.TagName;
+  self.AddDataEntry = function(dataEntry) {
+    if(dataEntry["AnswerCount"])
+      self.AnswerCount += parseInt(dataEntry.AnswerCount);
+    if(dataEntry["CommentCount"])
+      self.CommentCount += parseInt(dataEntry.CommentCount);
+    if(dataEntry["FavoriteCount"])
+      self.FavoriteCount += parseInt(dataEntry.FavoriteCount);
+    if(dataEntry["ViewCount"])
+      self.ViewCount += parseInt(dataEntry.ViewCount);
+    if(dataEntry["OwnerUserId"] && !userIds[dataEntry.OwnerUserId]) {
+      userIds[dataEntry.OwnerUserId] = true;
+      self.OwnerUserIdCount += 1;
+    }
+    if(dataEntry["Score"])
+      self.ScoreCount += parseInt(dataEntry.Score);
+  }
+  self.AnswerCount = 0;
+  self.CommentCount = 0;
+  self.FavoriteCount = 0;
+  self.ViewCount = 0;
+  self.OwnerUserIdCount = 0;
+  self.ScoreCount = 0;
+  self.AddDataEntry(dataEntry);
+  return self;
+}
+
+d3.csv("../data/QueryResults.csv", function(error, data) {
+
+// AnswerCount
+// CommentCount
+// FavoriteCount
+// ViewCount
+// OwnerUserId
+// Score
+
+var countFunc = (tag) => tag.FavoriteCount;
+
+var tags = {};
 
 data.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.value = +d.value;
+  if(tags[d.TagName]) {
+    tags[d.TagName].AddDataEntry(d);
+  } else {
+    tags[d.TagName] = tag(d);
+  }
 });
 
-x.domain(data.map(function(d) { return d.date; }));
-y.domain([0, d3.max(data, function(d) { return d.value; })]);
+var maxy = d3.max(Object.values(tags), countFunc);
+console.log(maxy);
+x.domain(Object.keys(tags));
+y.domain([0, maxy]);
 
 svg.append("g")
   .attr("class", "x axis")
@@ -58,12 +100,12 @@ svg.append("g")
   .text("Value ($)");
 
 svg.selectAll("bar")
-  .data(data)
+  .data(Object.values(tags))
 .enter().append("rect")
   .style("fill", "steelblue")
-  .attr("x", function(d) { return x(d.date); })
+  .attr("x", function(t) { return x(t.TagName); })
   .attr("width", x.rangeBand())
-  .attr("y", function(d) { return y(d.value); })
-  .attr("height", function(d) { return height - y(d.value); });
+  .attr("y", function(t) { return y(countFunc(t)); })
+  .attr("height", function(t) { return height - y(countFunc(t)); });
 
 });
