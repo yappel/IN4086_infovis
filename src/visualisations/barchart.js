07832cf477e7f7b8688dfce0dd86704c08d009f0
Yoeri Appel <y.a.appel@student.tudelft.nl>
2017-12-07 21:07:21 +0100

    Made barchart show multiple counts at once

--- src/visualisations/barchart.js
+++ src/visualisations/barchart.js
@@ -5,11 +5,9 @@ class BarChart extends BaseVisualisation {
 
     constructor(root, filterChangedCallback, data, options) {
         super(root, data);
-        if (options["countFunc"])
-            this.countFunc = options.countFunc;
-        else
-            this.countFunc = (tag) => tag.FavoriteCount;
 
+        var countsToDisplay = ["CommentCount", "OwnerUserIdCount", "AnswerCount", "FavoriteCount"];
+        this.countsToDisplay = countsToDisplay;
         var margin = {
                 top: 20,
                 right: 20,
@@ -21,13 +19,14 @@ class BarChart extends BaseVisualisation {
        
         this.height = height;
 
-        var tags = {};
-        data.forEach(function (d) {
-            if (!tags[d.TagName]) {
-                tags[d.TagName] = true;;
-            }
-        });
+        var tags = this.transformData(data);
 
+        var innerAxis = d3.scaleBand()
+            .padding(0.05);
+        this.innerAxis = innerAxis;
+        //Todo fix color scheme
+        var colorScheme = d3.scaleOrdinal().range(['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']);
+        this.colorScheme = colorScheme;
         var xChart = d3.scaleBand()
             .domain(Object.keys(tags).sort())
             .range([0, width])
@@ -63,48 +62,49 @@ class BarChart extends BaseVisualisation {
         .call(xAxis)
         .selectAll("text")
             .style("text-anchor", "end")
+            .style("font-size", "1.5em")
             .attr("dx", "-.8em")
             .attr("dy", ".15em")
             .attr("transform", function(d){
                 return "rotate(-65)";
             });
-
-        this.tag = function (dataEntry) {
-            var self = {};
-            var userIds = {}
-
-            self.TagName = dataEntry.TagName;
-            self.AddDataEntry = function (dataEntry) {
-                if (dataEntry["AnswerCount"])
-                    self.AnswerCount += parseInt(dataEntry.AnswerCount);
-                if (dataEntry["CommentCount"])
-                    self.CommentCount += parseInt(dataEntry.CommentCount);
-                if (dataEntry["FavoriteCount"])
-                    self.FavoriteCount += parseInt(dataEntry.FavoriteCount);
-                if (dataEntry["ViewCount"])
-                    self.ViewCount += parseInt(dataEntry.ViewCount);
-                if (dataEntry["OwnerUserId"] && !userIds[dataEntry.OwnerUserId]) {
-                    userIds[dataEntry.OwnerUserId] = true;
-                    self.OwnerUserIdCount += 1;
-                }
-                if (dataEntry["Score"])
-                    self.ScoreCount += parseInt(dataEntry.Score);
-            }
-            self.AnswerCount = 0;
-            self.CommentCount = 0;
-            self.FavoriteCount = 0;
-            self.ViewCount = 0;
-            self.OwnerUserIdCount = 0;
-            self.ScoreCount = 0;
-            self.AddDataEntry(dataEntry);
-            return self;
-        };
+        
+        this.dataGroup = this.svg.append("g");
+            
     }
 
+    tag(dataEntry) {
+        var self = {};
+        var userIds = {}
 
-    update(data, filtered_data, data_has_changed = false) {
-        super.update(data, filtered_data, data_has_changed);
-        console.log("Updating data for barchart...");
+        self.TagName = dataEntry.TagName;
+        self.AddDataEntry = function (dataEntry) {
+            if (dataEntry["AnswerCount"])
+                self.AnswerCount += parseInt(dataEntry.AnswerCount);
+            if (dataEntry["CommentCount"])
+                self.CommentCount += parseInt(dataEntry.CommentCount);
+            if (dataEntry["FavoriteCount"])
+                self.FavoriteCount += parseInt(dataEntry.FavoriteCount);
+            if (dataEntry["ViewCount"])
+                self.ViewCount += parseInt(dataEntry.ViewCount);
+            if (dataEntry["OwnerUserId"] && !userIds[dataEntry.OwnerUserId]) {
+                userIds[dataEntry.OwnerUserId] = true;
+                self.OwnerUserIdCount += 1;
+            }
+            if (dataEntry["Score"])
+                self.ScoreCount += parseInt(dataEntry.Score);
+        }
+        self.AnswerCount = 0;
+        self.CommentCount = 0;
+        self.FavoriteCount = 0;
+        self.ViewCount = 0;
+        self.OwnerUserIdCount = 0;
+        self.ScoreCount = 0;
+        self.AddDataEntry(dataEntry);
+        return self;
+    };
+
+    transformData(data) {
         var tags = {};
         var tag = this.tag;
         data.forEach(function (d) {
@@ -114,61 +114,48 @@ class BarChart extends BaseVisualisation {
                 tags[d.TagName] = tag(d);
             }
         });
+        return tags;
+    }
 
-        var maxy = d3.max(Object.values(tags), this.countFunc);
+
+    update(data, filtered_data, data_has_changed = false) {
+        super.update(data, filtered_data, data_has_changed);
+        console.log("Updating data for barchart...");
+        var tags = this.transformData(data);
+        var countsToDisplay = this.countsToDisplay;
+
+        var allcounts = Object.keys(tags).map(tag => countsToDisplay.map(m => tags[tag][m]));
+        var maxy = d3.max([].concat(...allcounts), i=>i);
         // var x = this.x;
         // var y = this.y;
         var countFunc = this.countFunc;
         var height = this.height;
-        var xAxis = this.xAxis;
         var yAxis = this.yAxis;
         var chart = this.svg;
         var yChart = this.yChart;
         var xChart = this.xChart;
-        var barWidth = this.barWidth;
+        var innerAxis = this.innerAxis;
+        var colorScheme = this.colorScheme;
 
+        innerAxis.domain(countsToDisplay).rangeRound([0, xChart.bandwidth()]);
         //Update y axis
         this.yChart.domain( [0, maxy] );
         chart.select(".yAxis").call(yAxis);
         
-        //select all bars on the graph, take them out, and exit the previous data set. 
-        //then you can add/enter the new data set
-        var bars = chart.selectAll(".bar")
-            .remove()
-            .exit()
+ 
+        this.dataGroup.selectAll("g")
             .data(Object.values(tags))
-        //now actually give each rectangle the corresponding data
-        bars.enter()
-            .append("rect")
-            .attr("class", "bar")
-            .attr("x", function (d, i) {
-                return xChart(d.TagName)
-            })
-            .attr("y", function (d) {
-                return yChart(countFunc(d));
-            })
-            .attr("height", function (d) {
-                return height - yChart(countFunc(d));
-            })
-            .attr("width", xChart.bandwidth())
-            .attr("fill", function (d) {
-                    return "orange";
-            });
-
-        // this.svg.selectAll("bar")
-        //     .data(Object.values(tags))
-        //     .enter().append("rect")
-        //     .style("fill", "steelblue")
-        //     .attr("x", function (t) {
-        //         return x(t.TagName);
-        //     })
-        //     .attr("width", x.rangeBand())
-        //     .attr("y", function (t) {
-        //         return y(countFunc(t));
-        //     })
-        //     .attr("height", function (t) {
-        //         return height - y(countFunc(t));
-        //     });
+            .enter().append("g")
+                .attr("transform", (d) => `translate(${xChart(d.TagName)},0)`)
+            .selectAll("rect")
+            .data((d) => countsToDisplay.map(m => ({measure:m, count :d[m]})))
+            .enter().append("rect")
+                .attr("x", (d) => innerAxis(d.measure))
+                .attr("y", (d) => yChart(d.count))
+                .attr("width", innerAxis.bandwidth())
+                .attr("height", (d) => height - yChart(d.count))
+                .attr("fill", (d) => colorScheme(d.measure));
+      
         
         console.log("Data for barChart updated!");
     }
