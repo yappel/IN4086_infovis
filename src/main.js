6b8a81afb403e3d0f6dba87f04a9db56bff4cfaf
Remco de Vos <vos.remcode@gmail.com>
2017-12-22 16:53:35 +0100

    Cleaned up main

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import * as d3 from "d3";
+import data_url from '../data/QueryResults.csv';
 import TagGraph from './visualisations/taggraph.js';
 import BarChart from './visualisations/barchart.js';
-import data_url from '../data/QueryResults.csv';
 import StackedChart from './visualisations/stackedchart.js';
 import StatisticsView from './visualisations/statisticsview.js';
 
+// Init variables
 var data = [];
 var filtered_data = [];
 var visualisations = [];
-var barchart;
 
+// data fields which are numeric
 var numeric_value_properties = ["Count", "Score", "ViewCount", "AnswerCount", "CommentCount", "FavoriteCount"];
 
+// Callback function used by visualisations on filter changes
 var filterCallback = function (initiator) {
     visualisations.forEach(vis1 => {
         var filtered_data = data;
@@ -21,18 +23,15 @@ var filterCallback = function (initiator) {
                 filtered_data = vis2.filter(filtered_data);
             } 
         });
+        // Don't call update on initiator
         if (vis1 !== initiator) vis1.update(data, filtered_data, false);
     });
 }
-window.updateshit = () => {
-    visualisations.forEach(vis => {
-        vis.update(data, filtered_data, true);
-    });
-}
-
+// Load in the data
 d3.csv(data_url, (d) => {
     data = d;
     filtered_data = d;
+    // Parse numeric data from string for each data entry
     data.forEach(data_element => {
         numeric_value_properties.forEach(id => {
             if (data_element[id]) {
@@ -40,37 +39,17 @@ d3.csv(data_url, (d) => {
             }
         });
     });
-
-    barchart = new BarChart(d3.select("#barchart"), filterCallback, data, {})
+    // Create and add the visualisations
+    var barchart = new BarChart(d3.select("#barchart"), filterCallback, data, {})
     visualisations.push(barchart);
-
     var graph = new TagGraph(d3.select("#taggraph"), filterCallback, {});
     visualisations.push(graph);
-     
     var stackedchart = new StackedChart(d3.select("#stackedchart"), filterCallback, data,{});
     visualisations.push(stackedchart);
-
     var statistics = new StatisticsView(d3.select("#statistics"), filterCallback);
     visualisations.push(statistics);
-
-    window.barchart = barchart;
-    window.taggraph = graph;
-    window.stackedchart = stackedchart;
-    window.visualisations = visualisations;
-    window.data = data;
-    window.updateshit();
+    // Call update on all visualisations
+    visualisations.forEach(vis => {
+        vis.update(data, filtered_data, true);
+    });
 });
-
-
-
-
-
-window.updateBarChart = (val, checked) => {
-    if (checked) {
-        barchart.enable(val);
-    }
-    else {
-        barchart.disable(val);
-    }
-}
-
